401cb05ae76760b7fb2156887c7c711548edfc28
akhilesoni <akhileshshroff@gmail.com>
2022-01-31 20:40:38 +0530

    hoping for magic on cors second time

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const bodyParser = require('body-parser')
 
 app.use(express.urlencoded({limit:'50mb',extended:true}))
 app.use(express.json({limit:'50mb'}))
+const corsOptions ={
+    origin:'*', 
+    credentials:true,           
+    optionSuccessStatus:200,
+ }
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use(routes)
 
