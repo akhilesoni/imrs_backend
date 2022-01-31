5a501d90975bbc27ed0d8e1b71ec882738cf3b17
akhilesoni <akhileshshroff@gmail.com>
2022-01-31 20:25:52 +0530

    hoping for magic on cors

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
-//dotenv
 require('dotenv').config()
 
-//router
 const routes = require('./routes')
-
-//dependencies
 const bodyParser = require('body-parser')
-const cors = require('cors')
+
 app.use(express.urlencoded({limit:'50mb',extended:true}))
 app.use(express.json({limit:'50mb'}))
-//app.use(bodyParser.json()); 
-//app.use(bodyParser.urlencoded({ extended: true })); 
 
-//cors
 app.use(cors())
 
-
-//routes
 app.use(routes)
-
 
 const port = process.env.PORT || 8000
 app.listen(port,()=>{
